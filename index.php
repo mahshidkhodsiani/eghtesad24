4407893a60f0b2dfdcb480312f9fca005c190333
mahshidkhodsiani <mahshidkhodsiani@yahoo.com>
2024-01-03 14:06:58 +0330

    004-new tables

--- index.php
+++ index.php
@@ -1811,56 +1811,7 @@ header('Content-Type: text/html; charset=utf-8');
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl-8">
-                                <div class="card overflow-hidden">
-                                    <div class="card-header border-0 pb-0 flex-wrap">
-                                        <h4 class="heading mb-0">Projects Overview</h4>
-                                        <ul class="nav nav-pills mix-chart-tab" id="pills-tab" role="tablist">
-                                            <li class="nav-item" role="presentation">
-                                                <button class="nav-link active" data-series="week" id="pills-week-tab"
-                                                    data-bs-toggle="pill" data-bs-target="#pills-week" type="button"
-                                                    role="tab" aria-selected="true">Week</button>
-                                            </li>
-                                            <li class="nav-item" role="presentation">
-                                                <button class="nav-link" data-series="month" id="pills-month-tab"
-                                                    data-bs-toggle="pill" data-bs-target="#pills-month" type="button"
-                                                    role="tab" aria-selected="false">Month</button>
-                                            </li>
-                                            <li class="nav-item" role="presentation">
-                                                <button class="nav-link" data-series="year" id="pills-year-tab"
-                                                    data-bs-toggle="pill" data-bs-target="#pills-year" type="button"
-                                                    role="tab" aria-selected="false">Year</button>
-                                            </li>
-                                            <li class="nav-item" role="presentation">
-                                                <button class="nav-link" data-series="all" id="pills-all-tab"
-                                                    data-bs-toggle="pill" data-bs-target="#pills-all" type="button"
-                                                    role="tab" aria-selected="false">All</button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="card-body  p-0">
-                                        <div id="overiewChart"></div>
-                                        <div class="ttl-project">
-                                            <div class="pr-data">
-                                                <h5>12,721</h5>
-                                                <span>Number of Projects</span>
-                                            </div>
-                                            <div class="pr-data">
-                                                <h5 class="text-primary">721</h5>
-                                                <span>Active Projects</span>
-                                            </div>
-                                            <div class="pr-data">
-                                                <h5>$2,50,523</h5>
-                                                <span>Revenue</span>
-                                            </div>
-                                            <div class="pr-data">
-                                                <h5 class="text-success">12,275h</h5>
-                                                <span>Working Hours</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                         
 
 
 
@@ -1905,16 +1856,100 @@ header('Content-Type: text/html; charset=utf-8');
 
                                 </div>
                             </div>
+
+
+                            <div class="col-xl-4">
+                                <div class="card">
+                                    <div class="card-header border-0">
+                                        <h2>قیمت ارز بازار</h2>
+
+                                    </div>
+
+
+                                    <div class="table-responsive">
+                                        <table class="table table-responsive-md w-100">
+                                            <thead>
+                                                <tr>
+                                                    <th>عنوان</th>
+                                                    <th>قیمت زنده</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                    $json_file = 'python-scripts/currencies_table.json';
+                                                    $json_data = file_get_contents($json_file);
+                                                    $data = json_decode($json_data, true);
+
+                                                    if (json_last_error() !== JSON_ERROR_NONE) {
+                                                        echo 'Error decoding JSON: ' . json_last_error_msg();
+                                                    } else {
+                                                        foreach ($data as $prices) {
+                                                            ?>
+                                                <tr>
+                                                    <td><strong><?= nl2br($prices[0]) ?></strong></td>
+                                                    <td><?= $prices[1] ?></td>
+                                                </tr>
+                                                <?php
+                                                        }
+                                                    }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div class="col-xl-4">
+                                <div class="card">
+                                    <div class="card-header border-0">
+                                        <h2>قیمت ارز بازار</h2>
+
+                                    </div>
+
+
+                                    <div class="table-responsive">
+                                        <table class="table table-responsive-md w-100">
+                                            <thead>
+                                                <tr>
+                                                    <th>عنوان</th>
+                                                    <th>قیمت زنده</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                    $json_file = 'python-scripts/currencies_table.json';
+                                                    $json_data = file_get_contents($json_file);
+                                                    $data = json_decode($json_data, true);
+
+                                                    if (json_last_error() !== JSON_ERROR_NONE) {
+                                                        echo 'Error decoding JSON: ' . json_last_error_msg();
+                                                    } else {
+                                                        foreach ($data as $prices) {
+                                                            ?>
+                                                <tr>
+                                                    <td><strong><?= nl2br($prices[0]) ?></strong></td>
+                                                    <td><?= $prices[1] ?></td>
+                                                </tr>
+                                                <?php
+                                                        }
+                                                    }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
+                            </div>
+
+
+
                         </div>
                     </div>
 
 
 
                     <div class="col-md-3 col-sm-12">
-
-
-
-
                         <div class="card">
                             <div class="card-header border-0">
                                 <h2>نمایه های کریپتو</h2>
